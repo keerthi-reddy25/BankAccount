e860e3ca122c139b6d5ddca1990db6a0f0d016a7
keerthi-reddy25 <kamireddykeerthireddy@gmail.com>
2026-02-26 19:21:49 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,16 @@
+public class Day1 {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double height = 5.4;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Lily";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(isStudent);
+    }
+}
