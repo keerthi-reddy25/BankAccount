12f14d84ec6d04b5863d7b7fb5448a28bdecf29c
keerthi-reddy25 <kamireddykeerthireddy@gmail.com>
2026-03-01 15:53:10 +0530

    Add files via upload

---
+++ oned.java
@@ -0,0 +1,9 @@
+public class oned{
+public static void main(String[] args){
+int[] arr={2,4,6,8,10};
+System.out.println("First:" +arr[0]);
+System.out.println("Last:" +arr[arr.length-1]);
+System.out.println("Length:" +arr.length);
+}
+}
+
