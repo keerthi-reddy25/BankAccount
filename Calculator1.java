3f55ad9ba8980106763ae4a40b8cd789a5aef2c0
keerthi-reddy25 <kamireddykeerthireddy@gmail.com>
2026-03-01 15:53:26 +0530

    Add files via upload

---
+++ Calculator1.java
@@ -0,0 +1,12 @@
+public class Calculator1{
+public static int multiply(int a,int b){
+return a*b;
+}
+public static int multiply(int a,int b,int c){
+return a*b*c;
+}
+public static void main(String[] args){
+System.out.println(multiply(2,3));
+System.out.println(multiply(2,3,4));
+}
+}
